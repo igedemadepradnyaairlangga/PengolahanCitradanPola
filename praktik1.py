0c2c8f74e546b29a3fac83b24ac1b918234d46fa
igedemadepradnyaairlangga <72331276+igedemadepradnyaairlangga@users.noreply.github.com>
2022-05-17 11:02:51 +0700

    MORFOLOGI - dilasi
    MORFOLOGI - dilasi

--- praktik1.py
+++ praktik1.py
@@ -1,16 +1,11 @@
 import cv2
 import numpy as np
-# read the image
-image = cv2.imread('banteng.jpg')
-# apply the 3x3 mean filter on the image
-kernel = np.ones((3,3),np.float32)/9
-processed_image = cv2.filter2D(image,-1,kernel)
-mean_blur = cv2.blur(image,(5,5))
-gaussian_blur = cv2.GaussianBlur(image,(5,5),0)
-# display image
-cv2.imshow('Gambar Asli Banteng', image)
-cv2.imshow('Mean Filter Processing', processed_image)
-cv2.imshow('Mean Blur', mean_blur)
-cv2.imshow('Gaussian Blur', gaussian_blur)
-# pause the execution of the script until a key on the keyboard is pressed
-cv2.waitKey(0)
+from matplotlib import pyplot as plt
+img = cv2.imread('test.jpeg')
+kernel = np.ones((5,5),np.uint8)
+dilation = cv2.dilate(img,kernel,iterations = 1)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dilation),plt.title('Dilation')
+plt.xticks([]), plt.yticks([])
+plt.show()
